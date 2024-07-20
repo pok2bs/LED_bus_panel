625f697e4a2b1d45e71d0bee75d780107175ce7d
geng ho <pok2.9633@gmail.com>
2024-07-20 11:12:01 +0900

    디버그

--- python/library/LEDPanel/test.py
+++ python/library/LEDPanel/test.py
@@ -5,12 +5,14 @@ print(1)
 from LEDPanel import Matrix, MatrixOption
 print(2)
 # Configuration for the matrix
+print(3)
 optioner = MatrixOption(debug_mode=False)
 optioner.set_panel_size((64, 32))
 optioner.set_for_rpi_4()
+print(4)
 
 matrix = Matrix(optioner.get_option(), debug_mode=False)
-
+print(5)
 # RGB example w/graphics prims.
 # Note, only "RGB" mode is supported currently.
 image = Image.new("RGB", (32, 32))  # Can be larger than matrix if wanted!!
@@ -19,12 +21,12 @@ draw = ImageDraw.Draw(image)  # Declare Draw instance before prims
 draw.rectangle((0, 0, 31, 31), fill=(0, 0, 0), outline=(0, 0, 255))
 draw.line((0, 0, 31, 31), fill=(255, 0, 0))
 draw.line((0, 31, 31, 0), fill=(0, 255, 0))
-
+print(6)
 # Then scroll image across matrix...
 matrix.draw_image(image, (0, 0))
 time.sleep(10)
 for n in range(-32, 33):  # Start off top-left, move off bottom-right
     matrix.draw_image(image, (n, n))
     time.sleep(0.05)
-
+print(7)
 matrix.clear()
