cb90b021cdb287cb306a8b02a73a9937122deed4
geng ho <pok2.9633@gmail.com>
2024-07-21 17:57:54 +0900

    test

--- python/example/test.py
+++ python/example/test.py
@@ -1,23 +1,30 @@
-import bus
+import bus, time
 from LEDPanel import ImagePanel
+from info_reciver import ArriveInfoReceiver
 
 buspanel = bus.BusPanel()
 panel = ImagePanel(resolution=(192, 64))
+key = 'eJO+CaSYpebfSPJI3R0BKMU9Ua2/LAVpnlqCZxlKH6XzKHL+eKC9CY6xrBJQA4croYCZ/6OFZEU2d3elBLsIbg=='
+cityCode = 21
+nodeid = "BSB164630302"
+while True:
+    bus_data = ArriveInfoReceiver(key=key, citycode=cityCode, nodeid=nodeid)
 
-buspanel.clear()
-panel.clear()
+    datas = bus_data.getStopArriveInfo()
+    buspanel.clear()
+    panel.clear()
 
-buspanel.add_bus('5511', time=10, lowdeck=True) #저상버스
-buspanel.add_bus('5513', time=15)
-buspanel.add_bus('5511', time=bus.BusPanel.end) #종료
-buspanel.add_bus('5516', time=bus.BusPanel.depot) #차고지
 
-buspanel.add_arrive('750A', density=2) #곧도착, 혼잡
-buspanel.add_arrive('750B', density=1) #곧도착, 보통
-buspanel.add_arrive('506', density=0) #곧도착, 여유
+    for data in datas:
+        arrive_time = int(int(data['arrtime'])/60)
+        if arrive_time > 3:
+            buspanel.add_bus(str(data['routeno']), time=arrive_time) #저상버스
+        else:
+            buspanel.add_arrive(str(data['routeno']))
 
-img = buspanel.get_image() #Pillow 이미지를 만들어서 가져옴
-panel.draw_image(img, (0,0))
+    img = buspanel.get_image() #Pillow 이미지를 만들어서 가져옴
+    panel.draw_image(img, (0,0))
+    time.sleep(30)
 
-import time
-time.sleep(100)
+    
+
