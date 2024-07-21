9b211cab23ab77f83420a1f57c0f6c169a01db71
geng ho <pok2.9633@gmail.com>
2024-07-21 14:24:05 +0900

    test 추가

--- python/library/LEDPanel/test_in_img.py
+++ python/library/LEDPanel/test_in_img.py
@@ -8,8 +8,8 @@ panel = ImagePanel(resolution=(192, 64))
 # RGB example w/graphics prims.
 # Note, only "RGB" mode is supported currently.
 # image = Image.new("RGB", (32, 32))  # Can be larger than matrix if wanted!!
-image = Image.open("test.jpeg", "r")
-image = image.resize((64, 64))
+image = Image.open("test.png", "r")
+image = image.resize((int(64*image.size[0]/image.size[1]), 64))
 # Then scroll image across matrix...
 panel.draw_image(image, offset=(0, 0))
 time.sleep(100)
