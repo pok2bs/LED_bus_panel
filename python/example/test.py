e0b02f64617f625ba8c228e536640632b2d3c30d
geng ho <pok2.9633@gmail.com>
2024-07-21 14:46:47 +0900

    example 코드 수정

--- python/example/test.py
+++ python/example/test.py
@@ -1,8 +1,8 @@
 import bus
-import xylopanel
+from LEDPanel.LEDPanel import ImagePanel
 
 buspanel = bus.BusPanel()
-panel = xylopanel.Panel(debug=True)
+panel = ImagePanel(resolution=(192, 64))
 
 buspanel.clear()
 panel.clear()
@@ -17,8 +17,7 @@ buspanel.add_arrive('750B', density=1) #곧도착, 보통
 buspanel.add_arrive('506', density=0) #곧도착, 여유
 
 img = buspanel.get_image() #Pillow 이미지를 만들어서 가져옴
-panel.draw_image((0, 0), img) #LED 패널 객체에 이미지를 그림
-panel.send() #LED 패널로 이미지 전송
+panel.draw_image(img, (0,0))
 
 import time
 time.sleep(100)
